1040e62c8d18065a7363279a72a61a1c130e3f26
- <116948697+flecik@users.noreply.github.com>
2023-05-01 14:49:23 +0200

    main logic done

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const part1 = document.querySelector(".rating");
 const part2 = document.querySelector(".after");
 const submit = document.querySelector(".rating__submit");
+const score = document.querySelector(".after__thx--score");
+
+const options = document.querySelectorAll(".rating__list--btn");
+
+let scoreVal = 0;
+
+options.forEach((option) =>
+    option.addEventListener("click", function () {
+        scoreVal = option.textContent;
+    })
+);
 
 submit.addEventListener("click", function () {
     part1.style.display = "none";
     part2.style.display = "block";
+
+    score.innerText = scoreVal;
 });
